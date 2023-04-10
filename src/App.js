23090415ef498fdb28549181605d0138a6be91fa
rakshit2208 <yadavrakshit290@gmail.com>
2023-04-10 19:47:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Wedding from "./Wedding";
+
+import About from "./About";
+import ContactUs from "./ContactUs";
+import Footer from "./Footer";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+import WhyUs from "./WhyUs";
+import Map from "./Map";
+// import Slider from "./Slider";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+      <Wedding/>
+      <About/>
+      <WhyUs/>
+      <ContactUs/>
+      <Map/>
+      <Footer/>
+      {/* <Slider/> */}
+    
+      
+      
+    </div>
+  );
+}
+
+export default App;
